1a5c212ff83f562d5f43339125830846fb726bbb
Linas <l.kovalciukas@gmail.com>
2025-03-25 09:28:22 +0200

    text-size update

--- js/task/text-size.js
+++ js/task/text-size.js
@@ -5,3 +5,85 @@ Reikalinga funkcija, kuri gauna teksa, kuri grazina rezultata, priklausomai nuo 
 - jei maziau 20 simboliu: "Tekstas is n simboliu."
 - jei ilgesnis: "Ilgas tekstas is n simboliu."
 */
+
+function textSize (text) {
+    if (text.length === 0) {
+        return "tuscias tekstas.";
+    } 
+    if (text.length < 10) {
+            return `trumpas tekstas is ${text.length} simboliu`;
+    }
+    if (text.length < 20) {
+        return `Tekstas is ${text.length} simboliu.`
+    }
+        // return 'trumpas tekstas is ${text.length} simboliu';
+        return `Ilgas tekstas is ${text.length} simboliu`
+   
+}
+
+console.log(textSize('')); // testukai del logikos
+console.log(textSize('Labas'));
+console.log(textSize('Virs desimt '));
+console.log(textSize('virst dvidesimt tuei but'));
+console.log('------------------------');
+
+function textSize2 (text) {
+    if (text.length === 0) {
+        return "tuscias tekstas.";
+    } 
+    else if (text.length < 10) {
+            return `trumpas tekstas is ${text.length} simboliu`;
+    }
+    else if (text.length < 20) {
+        return `Tekstas is ${text.length} simboliu.`
+    }
+        // return 'trumpas tekstas is ${text.length} simboliu';
+        return `Ilgas tekstas is ${text.length} simboliu`
+   
+} // 2 variantas kaip parasyti
+
+console.log(textSize('')); // testukai del logikos
+console.log(textSize('Labas'));
+console.log(textSize('Virs desimt '));
+console.log(textSize('virst dvidesimt tuei but'));
+console.log('------------------------');
+
+// svarbu eiliskumus tinkamai parasyk if <10, if >20 ir panasiai. eiliskumas nuo maziausio arba nuo didziausio
+
+function textSize4 (text) {
+    if (text.length < 20) {
+        return `Tekstas is ${text.length} simboliu.`;
+    }
+    if (text.length < 10) {
+        return `trumpas tekstas is ${text.length} simboliu`;
+    }
+    if (text.length === 0) {
+        return "tuscias tekstas.";
+    }
+}  
+console.log(textSize('')); // testukai del logikos
+console.log(textSize('Labas'));
+console.log(textSize('Virs desimt '));
+console.log(textSize('virst dvidesimt tuei but'));
+console.log('------------------------');
+
+function textSize5 (text) {  // best way to do it
+    if (text.length < 20) {
+        return `Tekstas is ${text.length} simboliu.`;
+    }
+    if (text.length > 0 && text.length < 10) {
+        return "tuscias tekstas.";
+    }
+    if (text.length >= 10 && text.length < 20 ) {
+        return `trumpas tekstas is ${text.length} simboliu`;
+    }
+    if (text.length === 0) {
+        return "tuscias tekstas.";
+    }
+}
+console.log(textSize('')); // testukai del logikos
+console.log(textSize('Labas'));
+console.log(textSize('Virs desimt '));
+console.log(textSize('virst dvidesimt tuei but'));
+console.log('------------------------');
+
