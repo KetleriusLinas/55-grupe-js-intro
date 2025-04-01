bae5882dd2ddef0d91e310a20bfbaec447703fd2
Linas <ketleriuslinas@gmail.com>
2025-04-01 20:16:47 +0300

    uduot+funkcijos update

--- praktika/uzduotys+funkcijos.js
+++ praktika/uzduotys+funkcijos.js
@@ -141,7 +141,7 @@ console.log(stringReverse('pasivaiksciojimaS'));
 console.log(stringReverse('123'));
 console.log(stringReverse('-25'));
 
-console.log('---------------7--------------');
+console.log('---------------7 a.--------------');
 
 // Sukurkite funkciją, kuri apskaičiuotų stačiakampio plotą.
 // a. Padaryti su sąrašo (array) tipo kintamuoju
@@ -156,7 +156,22 @@ const result = a * b;
 console.log(staciakampioPlotas(5, 7));
 console.log(staciakampioPlotas(7, 10));
 console.log(staciakampioPlotas(2, 6));
-console.log('---------pabaigti riekia masyvu-------------');
+
+console.log('---------------7 b.-- ne pats------------');
+function staciakampioPlotas1 (skaiciai) {
+
+let a = skaiciai[0];
+let b = skaiciai[1];
+    if (typeof a !== 'number' || typeof b !== 'number'){
+        return 'Turi buti skaiciai'
+}
+        return a * b;
+}
+// let skaiciai = [5, 5];
+let skaiciai = ['abc', 5];
+console.log(staciakampioPlotas1(skaiciai));
+
+
 
 console.log('---------------8--------------');
 
@@ -190,4 +205,115 @@ console.log('---------------9--------------');
 // Sukurkite funkciją, kuri tikrina, ar teksto tipo kintamasis prasideda raide 'a'.
 
 //startsWith -- kuo prasideda zodis/
+function kintamojoTikrinimas (zodis) {
+    if (typeof zodis !== 'string' || isFinite(zodis)) {
+        return `Netinkama reiksme`
+    }
+// let pirmaRaide = a;
 
+    // for (let i = 0; i < zodis.length; i++) {
+        if ( zodis[0] == 'a') {
+            return `Taip, string tipo kintamasis ${zodis} prasideda raide a`;
+            // return `Taip, string tipo kintamasis prasideda raide a`;
+        } else {
+            return `Ne, ${zodis}, prasideda kita raide`;
+    }
+    }
+// }
+// console.log('labas'.startsWith('a'));
+// console.log('asilas'.startsWith('a'));
+// console.log('pumpuras'.startsWith('a'));
+// console.log('aciu'.startsWith('a'));
+
+console.log (kintamojoTikrinimas('labas'));
+console.log (kintamojoTikrinimas('asilas'));
+console.log (kintamojoTikrinimas('pumpuras'));
+console.log (kintamojoTikrinimas('aciu'));
+
+// console.log (kintamojoTikrinimas(123));
+// console.log (kintamojoTikrinimas(Infinity));
+// console.log (kintamojoTikrinimas(null));
+// console.log (kintamojoTikrinimas(true));
+
+// Sukurkite funkciją trijų skaičių vidurkiui rasti.
+console.log('---------------10--------------');
+
+function skaiciuVidurkis (a, b, c) {
+    if (typeof a !== 'number' || !isFinite(a)) { // isFinite tikrina ar baigtinis skaicius (!) reik
+        return `netinkama reiksme: ${a}`;
+    } if (typeof b !== 'number' || !isFinite(b)) {
+        return `netinkama reiksme: ${b}`;
+    }if (typeof c !== 'number' || !isFinite(c)) {
+        return `netinkama reiksme: ${c}`;
+    }
+// const result = (a + b + c) / 3;
+    return (a + b + c) / 3;
+}
+console.log(skaiciuVidurkis(3, 3, 3));
+console.log(skaiciuVidurkis(13, 2, 10));
+console.log(skaiciuVidurkis(-8, -17, -12));
+// console.log(skaiciuVidurkis('abc', 9, 15));
+// console.log(skaiciuVidurkis(13, 'abc', 15));
+// console.log(skaiciuVidurkis(5, 9, 'abc'));
+// console.log(skaiciuVidurkis(true, 9, 15));
+// console.log(skaiciuVidurkis(13, true, 15));
+// console.log(skaiciuVidurkis(5, 9, true));
+// console.log(skaiciuVidurkis(NaN, 9, 15));
+// console.log(skaiciuVidurkis(13, NaN, 15));
+// console.log(skaiciuVidurkis(5, 9, NaN));
+console.log('---------------11--------------');
+
+// . Sukurkite funkciją, grąžinančią teksto tipo kintamojo ilgį
+function kintamojoIlgis (text) {
+    if (typeof text !== 'string' || isFinite(text)) {
+        return `bloga reiksme`
+    }
+    return text.length;
+}
+console.log(kintamojoIlgis('labas'));
+console.log(kintamojoIlgis('rabarbaras'));
+console.log(kintamojoIlgis('vakaras'));
+
+// console.log(kintamojoIlgis(-Infinity));
+// console.log(kintamojoIlgis(Infinity));
+// console.log(kintamojoIlgis(true, 9, 15));
+// console.log(kintamojoIlgis(true));
+// console.log(kintamojoIlgis());
+// console.log(kintamojoIlgis(-15));
+// console.log(kintamojoIlgis(NaN));
+console.log('---------------12--------------');
+
+// 12. Sukurkite funkciją dviem eilutėms sujungti
+
+function eilutesSujungti (a, b) {
+// const sum = a + b;
+    return a + '' + b;
+}
+console.log(eilutesSujungti('Siandien, sviecia saule', 'vakaras, tikrai, kad vakaras'));
+console.log(eilutesSujungti('labas, as esu Linas', ' vakaras, tikrai, kad vakaras'));
+console.log(eilutesSujungti('ryt ryte, ridensis rutulis', 'diena, tai ne vakaras'));
+console.log(eilutesSujungti(12, 15));
+
+console.log('---------------13--------------');
+// 13. Sukurkite funkciją, kuri tikrina, ar skaičius yra tarp dviejų reikšmių (imtinai).
+
+function tikrinaTarpReiksmiu (skaicius) {
+    if (typeof skaicius !== 'number' || !isFinite(skaicius)) {
+        return 'Ne skaicius'
+    }
+    if (10 <=  skaicius && skaicius <= 20 ){
+        return 'Taip, skaicius tinka';
+    }   if (skaicius > 20) {
+        return 'Netinka, perdidelis';
+    }   if (skaicius < 10) {
+        return 'Netinka, permazas';
+    }
+}
+console.log(tikrinaTarpReiksmiu(10));
+console.log(tikrinaTarpReiksmiu(-15));
+console.log(tikrinaTarpReiksmiu(15));
+// console.log(tikrinaTarpReiksmiu(true));
+// console.log(tikrinaTarpReiksmiu(Infinity));
+// console.log(tikrinaTarpReiksmiu(NaN));
+// console.log(tikrinaTarpReiksmiu('abc'));
+// console.log(tikrinaTarpReiksmiu(null));
