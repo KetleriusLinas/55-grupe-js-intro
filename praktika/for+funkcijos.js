e64d7f88d6801beea2a82347f288d3adca946dc1
Linas <ketleriuslinas@gmail.com>
2025-04-04 09:01:49 +0300

    mentores naujos for uzduotys

---
+++ praktika/for+funkcijos.js
@@ -0,0 +1,17 @@
+// Parašykite for ciklą, kuris apskaičiuotų visų skaičių nuo 5 iki 15 sumą
+console.log('---------------1----------------');
+let sum = 0;
+for (let i = 5; i < 15; i++) {
+    sum += i;
+    // console.log(sum);
+}
+console.log(sum);
+console.log('---------------2----------------');
+
+// 2. Parašykite for ciklą, kuris į console išvestų visas žodžio ‘Vakaras’ raides po vieną
+let zodis = 'Vakaras';
+for (let i = 0; i < zodis.length; i++) {
+    console.log(zodis[i]);
+}
+console.log('---------------3----------------');
+// 3. Parašykite for ciklą, kuris išvestų šešeto daugybos lentelę
