abeaf9b591967d4e8451fc83ba6768b0824410f6
Linas <ketleriuslinas@gmail.com>
2025-04-08 09:08:18 +0300

    ternary upd

--- js/15-ternary.js
+++ js/15-ternary.js
@@ -40,6 +40,12 @@ console.log(type);
 // visi skaiciai yra pozityvus ( visi isveda Taip) -- true
 // isskyrus nuli                (0 isveda ne)      -- false
 
+if (5) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
 // visi tekstai(stringai) yra pozityvus ( gali buti ir simboliai vistiek skaitos kaip ne tuscias tekstas)
 // isskyrus tuscia (' '); 
 
@@ -48,3 +54,54 @@ if ('') {
 } else {
     console.log('Ne');
 }
+
+// visi array yra pozityvus []
+// visi object yra pozityvus {}
+if ([]) {
+    console.log('taip');
+} else {
+    console.log('Ne');
+}
+
+// visi undefined yra negatyvus
+// visi NaN yra negatyvus
+if (NaN) {
+    console.log('Taip');
+} else {
+    console.log('Ne');
+}
+
+console.log('--------------------------------');
+
+
+//a = 0
+// if (1)
+//else (2)         
+
+// galima issimetyti ( issiskaityti lenviau skaityt)
+const a = 0 ? 1 : 2;
+console.log(a);
+
+const b = 1 ? 2 : 3;
+console.log(b);
+
+const c = 2 ? 3 ? 4 : 5 : 6;
+console.log(c);
+
+const d = 0 ? 1 : 2 ? 3 : 4;
+console.log(d);
+
+// bet taip nedaryti naudoti normalius if else.. tik 'vienam klausimui ternary', ar taip.. ar taip..
+const e = 1 
+? 2  // if 
+    ? 3 //if
+    : 4 //else
+: 5  // else
+    ? 6 //if
+    : 7; // else
+console.log(e);
+
+//unari (vienas) =
+// binari (du) = +, -, *, /
+//ternary (trys) = ? :
+
